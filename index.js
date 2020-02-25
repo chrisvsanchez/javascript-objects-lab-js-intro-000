5aa8eaed1ff83e9f5fdaa707103dea942927004f
Chris Sanchez <chrisvsanchez@gmail.com>
2020-02-25 18:46:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ function updateObjectWithKeyAndValue(object, key, value){
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
- let recipes =  Object.assign({}, object, {[key]: value});
+ object[key]= value;
  return recipes;
 }
