b6bd913b4e6e2f2c6a54f3bb2cc504989780a2a6
Chris Sanchez <chrisvsanchez@gmail.com>
2020-02-25 18:29:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var recipes = {};
 
 function updateObjectWithKeyAndValue(object, key, value){
-  var obj = object.assign({},[key], value);
+  var obj = object.assign({},key, value);
   return obj;
 }
