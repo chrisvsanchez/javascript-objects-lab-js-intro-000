cf93e3cafd3bb2cab01846b26cab61c42738fb1d
Chris Sanchez <chrisvsanchez@gmail.com>
2020-02-25 18:54:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 }
 
 function deleteFromObjectByKey(object, key){
-  let newObj = Object.assign({},object ;) 
+  let newObj = Object.assign({},object);
   delete object.key;
   return newObj;
 }
